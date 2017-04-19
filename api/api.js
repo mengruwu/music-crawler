a74fa86d5f6c78d0c3b89920896ab4e15e209eae
mengruwu <ray7102ray7102@gmail.com>
2017-04-19 22:40:12 +0800

    2017-4-19

---
+++ api/api.js
@@ -0,0 +1,9 @@
+var Kkbox = require('./kkbox/kkbox.js');
+
+class api {
+	constructor(app) {
+		app.get('/kkbox', function(req, res) {
+		});
+	}
+}
+module.exports = api;
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+
 var app = express();
+
+//var Api = require('./api/api.js');
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,4 +23,16 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+app.get('/kkbox', function(req, res) {
+	var songs = [];
+	request('http://www.kkbox.com/tw/tc/charts/western-daily-song-latest.html', function(error, response, body) {
+		if (error) throw new Error(error);
+		var $ = cheerio.load(body);
+		$('div.item h4 a').each(function(index, element) {
+			var song = {};
+			song.title = $(this).attr('title');
+			songs.push(song);
+		});
+		res.send(JSON.stringify(songs));
+	});
+});
